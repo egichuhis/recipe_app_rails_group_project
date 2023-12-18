7d5fac8225b2404d34d7d596d65eac7b82585ff8
AlselwiAisha <123330794+AlselwiAisha@users.noreply.github.com>
2023-12-18 13:51:17 +0300

    Create Models and database

---
+++ db/migrate/20231218100850_create_recipes_foods.rb
@@ -0,0 +1,11 @@
+class CreateRecipesFoods < ActiveRecord::Migration[7.1]
+  def change
+    create_table :recipes_foods do |t|
+      t.integer :quantity
+      t.references :food, null:false, foreign_key: {to_table: :foods}
+      t.references :recipe, null:false, foreign_key: {to_table: :recipes}
+
+      t.timestamps
+    end
+  end
+end
