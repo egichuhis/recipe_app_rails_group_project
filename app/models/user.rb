24466f24a195cd81291fbea4268d999f3040dbfe
AlselwiAisha <123330794+AlselwiAisha@users.noreply.github.com>
2023-12-18 14:09:14 +0300

    Fix linters errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-    has_many :foods, dependent: :destroy
-    has_many :recipes, dependent: :destroy
-    has_many :recipe_foods, through: :recipes
-  
-    validates :name, :email, presence: true
+  has_many :foods, dependent: :destroy
+  has_many :recipes, dependent: :destroy
+  has_many :recipe_foods, through: :recipes
+
+  validates :name, :email, presence: true
 end
