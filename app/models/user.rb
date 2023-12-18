2679c003c1428294e678cc7849b424b188d89bd6
AlselwiAisha <123330794+AlselwiAisha@users.noreply.github.com>
2023-12-18 21:41:41 +0300

    Fix linters errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :confirmable, :validatable
-         has_many :foods, dependent: :destroy
-         has_many :recipes, dependent: :destroy
-         has_many :recipe_foods, through: :recipes
-       
-         validates :name, :email, presence: true
+  has_many :foods, dependent: :destroy
+  has_many :recipes, dependent: :destroy
+  has_many :recipe_foods, through: :recipes
+
+  validates :name, :email, presence: true
 end
