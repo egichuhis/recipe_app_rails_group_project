ad0bcb398bb66e75d58c6aed95cd19f879ccc510
Edwin Gichuhi <edwingichuhis@gmail.com>
2023-12-19 14:49:09 +0300

    Add navigation links to nav buttons

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,20 @@ Rails.application.routes.draw do
 
   authenticated :user do
     resources :foods
-    resources :recipes
+    resources :recipes do
+      collection do
+        get 'public_recipes', to: 'recipes#public_recipes', as: 'public_recipes'
+        get 'private_recipes', to: 'recipes#private_recipes', as: 'private_recipes'
+      end
+    end
   end
 
-  root 'recipes#index'
+  # Change the root path to '/foods'
+  root 'foods#index'
+
+  # Map '/public_recipes' to 'recipes#public_recipes'
+  get '/public_recipes', to: 'recipes#public_recipes', as: 'public_recipes'
+
+  # Map '/recipes' to 'recipes#private_recipes'
+  get '/private_recipes', to: 'recipes#private_recipes', as: 'private_recipes'
 end
