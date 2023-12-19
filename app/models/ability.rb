bc9567ca2e783e2b28be8118805ccfc66eff69c7
AlselwiAisha <123330794+AlselwiAisha@users.noreply.github.com>
2023-12-19 11:32:05 +0300

    Add ability.rb file

---
+++ app/models/ability.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    user ||= User.new
+    if user.admin?
+      can :manage, :all
+    else
+      can :manage, Food, user_id: user.id
+      can :manage, Recipe, user_id: user.id
+      can :manage, RecipesFood, recipe: { user_id: user.id }
+      can :read, :all
+    end
+  end
+end
