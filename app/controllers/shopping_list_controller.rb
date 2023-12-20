376125aff7f178e8d0014d1a3950d077e6fcaf08
Edwin Gichuhi <edwingichuhis@gmail.com>
2023-12-21 00:51:12 +0300

    Refactor calculate_missing_foods method and update total price calculation

--- app/controllers/shopping_list_controller.rb
+++ app/controllers/shopping_list_controller.rb
@@ -6,19 +6,31 @@ class ShoppingListController < ApplicationController
     @user = current_user
     @missing_foods = calculate_missing_foods(@user)
     @total_items = @missing_foods.count
-    @total_price = calculate_total_price(@missing_foods)
-
-    # Additional logic as needed...
+    @general_total_price = calculate_total_price(@missing_foods)
   end
 
   private
 
   def calculate_missing_foods(user)
-    # Implement logic to find missing foods for the user's recipes
-    # You'll need to compare the general food list of the user with the foods in all recipes
+    # Find all foods associated with the user
+    user_foods = user.foods.to_a
+
+    # Find all foods associated with the user's recipes
+    recipe_foods = Recipe.joins(:recipe_foods).where(recipe_foods: { food: user_foods })
+
+    # Identify deficit items
+    deficit_items = []
+    recipe_foods.each do |recipe|
+      recipe.recipe_foods.each do |rf|
+        user_food = user_foods.find { |uf| uf == rf.food }
+        deficit_items << user_food if user_food.quantity < rf.quantity
+      end
+    end
+
+    deficit_items
   end
 
   def calculate_total_price(missing_foods)
-    # Implement logic to calculate the total price of missing foods
+    missing_foods.sum(&:price)
   end
 end
