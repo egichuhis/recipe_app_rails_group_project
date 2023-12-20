425f3e0f032a69e74e702f90fd1e4e5065cff9cf
AlselwiAisha <123330794+AlselwiAisha@users.noreply.github.com>
2023-12-20 18:35:53 +0300

    Update user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,30 @@
+# spec/features/user_registration_spec.rb
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.feature 'User Registration', type: :feature do
+  before :each do
+    @user = User.new(name: 'Adwin', email: 'Adwin@mail.com', password: 'Adwin')
+  end
+
+  it 'validates name is there' do
+    @user.name = nil
+    expect(@user).not_to be_valid
+  end
+
+  it 'validates email is there' do
+    @user.email = nil
+    expect(@user).not_to be_valid
+  end
+
+  context 'association' do
+    it 'has many foods' do
+      temp = User.reflect_on_association(:foods)
+      expect(temp.macro).to eq(:has_many)
+    end
+
+    it 'has many recipes' do
+      temp = User.reflect_on_association(:recipes)
+      expect(temp.macro).to eq(:has_many)
+    end
+  end
 end
